198b82179764dad0568ba9248dfc4f6c51b05d97
Adrien 42 <adr.bidaux@gmail.com>
2026-07-16 23:43:49 +0200

    Correction de l'accès aux dossiers d'examen

---
+++ fbkeskin-exam_05/level0/bigint/bigint.hpp
@@ -0,0 +1,104 @@
+// #ifndef BIGINT
+// #define BIGINT
+
+// #include <sstream>
+// #include <iostream>
+// #include <string>
+// #include <cstdlib>
+
+// class bigint {
+// 	private:
+// 		std::string str;
+// 		//std::string result;
+// 	public:
+// 		bigint();
+// 		bigint(unsigned int num);
+// 		bigint(const bigint& source);
+
+// 		std::string getStr()const;
+
+// 		bigint& operator=(const bigint& source);
+
+// 		// addition
+// 		bigint operator+(const bigint& other)const;
+// 		bigint& operator+=(const bigint& other);
+
+// 		// increments
+// 		bigint operator++(int); // x++
+// 		bigint& operator++(); // ++x
+
+// 		// shift with num
+// 		bigint operator<<(unsigned int n)const;
+// 		bigint operator>>(unsigned int n)const;
+// 		bigint& operator<<=(unsigned int n);
+// 		bigint& operator>>=(unsigned int n);
+
+// 		//shift with object
+// 		bigint operator<<(const bigint& other)const;
+// 		bigint operator>>(const bigint& other)const;
+// 		bigint& operator<<=(const bigint& other);
+// 		bigint& operator>>=(const bigint& other);
+
+// 		// ==, !=, <, >, <=, >=
+// 		bool operator==(const bigint& other) const;
+// 		bool operator!=(const bigint& other) const;
+// 		bool operator<(const bigint& other) const;
+// 		bool operator>(const bigint& other) const;
+// 		bool operator<=(const bigint& other) const;
+// 		bool operator>=(const bigint& other) const;
+
+// 		// ~bigint();
+
+// };
+
+// std::ostream& operator<<(std::ostream& output, const bigint& obj);
+
+// #endif
+
+
+
+// ------------------------------------------------------------
+// ---------------- C version ---------------------------------
+// ------------------------------------------------------------
+
+
+
+#pragma once
+#include <iostream>
+#include <string>
+
+class bigint {
+	std::string s;
+	public:
+		bigint(unsigned int n = 0);
+		bigint(const bigint &o);
+		~bigint();
+		
+		const std::string &str() const;
+		bigint &operator=(const bigint &o);
+		
+		// add
+		bigint operator+(const bigint &o) const;
+		bigint &operator+=(const bigint &o);
+		
+		//increment
+		bigint operator++(int);
+		bigint &operator++();
+		
+		//with num
+		bigint operator<<(unsigned int n) const;
+		bigint &operator<<=(unsigned int n);
+		
+		// with obj
+		bigint operator>>(const bigint &o) const;
+		bigint &operator>>=(const bigint &o);
+		
+		//bool
+		bool operator<(const bigint &o) const;
+		bool operator<=(const bigint &o) const;
+		bool operator>(const bigint &o) const;
+		bool operator>=(const bigint &o) const;
+		bool operator==(const bigint &o) const;
+		bool operator!=(const bigint &o) const;
+};
+std::ostream &operator<<(std::ostream &os, const bigint &n);
---
+++ my-exam_05/level0/bigint/bigint.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+
+class bigint {
+    std::string s;
+    private :
+        bigint(unsigned int n = 0);
+        bigint(const bigint &o);
+        ~bigint();
+
+        const std::string &str() const;
+        bigint &operator=(const bigint &o);
+
+        //add
+        bigint operator+(const bigint &o)const;
+        bigint &operator+=(const bigint &o);
+
+        //incr
+        bigint operator++(int);
+        bigint &operator++();
+
+        //with n
+        bigint operator<<(unsigned int n)const;
+        bigint operator<<=(unsigned int n);
+
+        //with obj
+        bigint operator>>(const bigint &o)const;
+        bigint operator>>=(const bigint &o);
+
+        //bool
+        bool operator<(const bigint &o)const;
+        bool operator<=(const bigint &o)const;
+        bool operator>(const bigint &o)const;
+        bool operator>=(const bigint &o)const;
+        bool operator==(const bigint &o)const;
+        bool operator!=(const bigint &o)const;
+};
+
+std::ostream &operator<<(std::ostream &os, const bigint &n);
